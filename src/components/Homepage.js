db13ce624f80beeb1fa79b1f6e0d01057c1e3c17
Aneta <anc@praqma.net>
2020-04-13 21:42:09 +0200

    project created

---
+++ src/components/Homepage.js
@@ -0,0 +1,48 @@
+import React from "react";
+import "../main.scss";
+
+const Homepage = () => {
+  return (
+    <div className="homepage">
+      <div className="menu">
+        <div className="logo">
+          <img src="" />
+        </div>
+      </div>
+      <div className="directory-menu">
+        <div className="menu-item">
+          <div className="content">
+            <h2>Rousky</h2>
+            <h3>Shop now</h3>
+          </div>
+        </div>
+        <div className="menu-item">
+          <div className="content">
+            <h2>Pradlo</h2>
+            <h3>Shop now</h3>
+          </div>
+        </div>
+        <div className="menu-item">
+          <div className="content">
+            <h2>Textil</h2>
+            <h3>Shop now</h3>
+          </div>
+        </div>
+        <div className="menu-item">
+          <div className="content">
+            <h2>Textil</h2>
+            <h3>Shop now</h3>
+          </div>
+        </div>
+        <div className="menu-item">
+          <div className="content">
+            <h2>Textil</h2>
+            <h3>Shop now</h3>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Homepage;
