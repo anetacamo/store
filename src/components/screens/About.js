9bdd7f1c350b884dfb4d86f58aa5ef84124b3654
Aneta <anc@praqma.net>
2020-04-30 03:46:46 +0200

    added an about section

--- src/components/screens/About.js
+++ src/components/screens/About.js
@@ -5,27 +5,48 @@ import coffee from "../../images/anetacamo_people.png";
 
 const About = () => {
   return (
-    <div>
-      <h2>About Page</h2>
+    <div style={{ maxWidth: 500 }}>
+      <h2>About this fake store</h2>
       <p>
-        I have built this small store just to get better at working with react.
-        It is filled up with random placeholders for now, although one day I
-        would love to use it for selling some of my products.
+        I build this store as a excercise to get more used to some React
+        features. It is filled up with random placeholders for now. I was
+        semi-following a tutorial on Udemy.com and the images I have downloaded
+        from the data to populate the store with items they have provided in the{" "}
+        <b>
+          <a
+            target="_blank"
+            href="https://www.udemy.com/course/complete-react-developer-zero-to-mastery"
+          >
+            course{" "}
+          </a>
+        </b>
         <br />
         <br />
-        For navigation I used react-router-dom.
+        For navigation I used <b>react-router-dom</b>.
         <br />
-        <br />I implemented Firebase for storing data about users and their cart
-        items that are updating live. Cart items only have limited amount in
-        stock so they update live as some of them might be bought. <br />
+        <br />I implemented <b>Firebase</b> for storing data about users and
+        their cart. Cart items only have limited amount in stock
         <br />
-        For payment I used a Stripe payment. It is implemented at the checkout
+        <br />
+        For cascading stylesheets I have been using sass <br />
+        <br />
+        For payment I used <b>Stripe</b>. It is implemented at the checkout
         page, it works but only as a test version so none real payments can be
         performed. <br />
         <br />
-        This project might have been pretty suitable for Redux implementation
-        but I decided to skip it for now as I really wanted focus on react
-        itself and its features.
+        This project have been continuously published to{" "}
+        <b>
+          <a href="https://github.com/anetacamo/store" target="_blank">
+            this github repo.
+          </a>
+        </b>
+      </p>
+      <p>
+        For deplyment I am using <b>Heroku</b> where the project is run as you
+        can see in the browser.
+        <br />
+        <br />
+        <b>There is still a lot of work to be done.</b>
       </p>
       <img src={coffee} alt="logo" style={{ maxWidth: 120 }} />
     </div>
